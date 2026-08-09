b13e9afff8115737d306a6996552e6b35e46f83f
Gaalbu <gabrielalbuquerquealencar@gmail.com>
2026-08-09 00:05:15 -0300

    chore: restore TypeScript lint and type checks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 module.exports = {
+  ignorePatterns: ['dist'],
   env: {
     browser: true,
     es2021: true
   },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:react-refresh/recommended',
     'prettier'
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-refresh'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -23,6 +26,7 @@ module.exports = {
     }
   },
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
   }
 };
